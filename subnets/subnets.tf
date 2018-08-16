1162e73b278670f5ad431acd9d5b9d52b3a0e29b
Rob Lazzurs <rob@lazzurs.org>
2018-08-16 21:23:42 +0100

    Ensure the subnets will split over the AZ list.
    The subnets were being automatically split by three rather than
    the specified number of AZs.

--- subnets/subnets.tf
+++ subnets/subnets.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "public_subnet" {
   count             = "${length(var.az_list)}"
-  cidr_block        = "${cidrsubnet(var.public_subnet,3 ,count.index )}"
+  cidr_block        = "${cidrsubnet(var.public_subnet,length(var.az_list) ,count.index )}"
   availability_zone = "${var.az_list[count.index]}"
   vpc_id            = "${data.aws_vpc.vpc.id}"
   tags              = "${merge(var.tags, map("Name", "${local.environment_name}_public", "Type", "public"))}"
@@ -8,7 +8,7 @@ resource "aws_subnet" "public_subnet" {
 
 resource "aws_subnet" "private_subnet" {
   count             = "${length(var.az_list)}"
-  cidr_block        = "${cidrsubnet(var.private_subnet,3 ,count.index )}"
+  cidr_block        = "${cidrsubnet(var.private_subnet,length(var.az_list) ,count.index )}"
   availability_zone = "${var.az_list[count.index]}"
   vpc_id            = "${data.aws_vpc.vpc.id}"
   tags              = "${merge(var.tags, map("Name", "${local.environment_name}_private", "Type", "private"))}"
@@ -16,7 +16,7 @@ resource "aws_subnet" "private_subnet" {
 
 resource "aws_subnet" "db_subnet" {
   count             = "${length(var.az_list)}"
-  cidr_block        = "${cidrsubnet(var.db_subnet,3 ,count.index )}"
+  cidr_block        = "${cidrsubnet(var.db_subnet,length(var.az_list) ,count.index )}"
   availability_zone = "${var.az_list[count.index]}"
   vpc_id            = "${data.aws_vpc.vpc.id}"
   tags              = "${merge(var.tags, map("Name", "${local.environment_name}_db", "Type", "db"))}"
