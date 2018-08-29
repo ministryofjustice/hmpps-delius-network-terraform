984bbf4fcef5f9054effcea85bb940408e68b0ce
smjmoj <smjmoj@gmail.com>
2018-08-29 19:11:26 +0100

    Enabled route tables to identify the AZ in the tag name.

--- routes/private.tf
+++ routes/private.tf
@@ -1,5 +1,5 @@
 resource "aws_route_table" "private" {
   count  = "${length(var.az_list)}"
   vpc_id = "${data.aws_vpc.vpc.id}"
-  tags   = "${merge(var.tags, map("Name", "${local.environment_name}_private"))}"
+  tags   = "${merge(var.tags, map("Name", "${local.environment_name}_private_${element(var.az_list, count.index)}"))}"
 }
