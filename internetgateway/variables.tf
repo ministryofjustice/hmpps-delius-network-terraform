d0b41f17bfea8cd9250f49579b701d4a8807d080
smjmoj <smjmoj@gmail.com>
2018-09-07 10:58:03 +0100

    Added internet gateway

---
+++ internetgateway/variables.tf
@@ -0,0 +1,15 @@
+variable "environment_name" {
+  type = "string"
+}
+
+variable "region" {
+  description = "The AWS region."
+}
+
+variable "remote_state_bucket_name" {
+  description = "Terraform remote state bucket name"
+}
+
+variable "environment_identifier" {
+  description = "resource label or name"
+}
