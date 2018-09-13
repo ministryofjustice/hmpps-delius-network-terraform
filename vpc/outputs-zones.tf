ea4d111259c38c3f3efb660e103454f71aaa47af
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2018-09-13 11:44:40 +0100

    Added public domain as an output

--- vpc/outputs-zones.tf
+++ vpc/outputs-zones.tf
@@ -9,3 +9,7 @@ output "private_zone_id" {
 output "public_zone_id" {
   value = "${data.aws_route53_zone.public_hosted_zone.zone_id}"
 }
+
+output "public_zone_name" {
+  value = "${local.public_domain}"
+}
