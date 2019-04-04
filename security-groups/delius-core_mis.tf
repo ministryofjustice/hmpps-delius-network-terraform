3de6cd5ef908286a4e7ed46f86a1abe01d77dda8
smjmoj <smjmoj@gmail.com>
2019-04-04 10:47:46 +0100

    Added two security groups one for Delisu Db to accept ingress from the MIS egress security group. Along with the two rules to support that.

--- security-groups/delius-core_mis.tf
+++ security-groups/delius-core_mis.tf
@@ -1,36 +1,54 @@
 # define security groups only for delius-core to from mis
 
-resource "aws_security_group" "delius_core_mis_db_in_out" {
-  name        = "${var.environment_name}-delius-core-mis-db-in-out"
+## Apply this SG to Delius DB to enable connection from MIS
+resource "aws_security_group" "delius_core_db_in_from_mis" {
+  name        = "${var.environment_name}-delius-core-db-in-from-mis"
   vpc_id      = "${data.terraform_remote_state.vpc.vpc_id}"
-  description = "Database in from MIS"
-  tags        = "${merge(var.tags, map("Name", "${var.environment_name}-delius-core-mis-db-in-out", "Type", "DB"))}"
+  description = "Delius Database in from MIS"
+  tags        = "${merge(var.tags, map("Name", "${var.environment_name}-delius-core-db-in-from-mis", "Type", "DB"))}"
 
   lifecycle {
     create_before_destroy = true
   }
 }
 
-output "sg_delius_core_mis_db_in_out_id" {
-  value = "${aws_security_group.delius_core_mis_db_in_out.id}"
+output "sg_delius_core_db_in_from_mis_id" {
+  value = "${aws_security_group.delius_core_db_in_from_mis.id}"
 }
 
-resource "aws_security_group_rule" "delius_core_mis_db_in" {
-  security_group_id = "${aws_security_group.delius_core_mis_db_in_out.id}"
-  type              = "ingress"
-  protocol          = "tcp"
-  from_port         = "1521"
-  to_port           = "1521"
-  self              = true
-  description       = "TF - MIS DB in"
+resource "aws_security_group_rule" "delius_core_db_in_from_mis" {
+  security_group_id        = "${aws_security_group.delius_core_db_in_from_mis.id}"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = "1521"
+  to_port                  = "1521"
+  source_security_group_id = "${aws_security_group.mis_out_to_delius_db.id}"
+  description              = "TF - Delius DB in from MIS"
 }
 
-resource "aws_security_group_rule" "delius_core_mis_db_out" {
-  security_group_id = "${aws_security_group.delius_core_mis_db_in_out.id}"
-  type              = "egress"
-  protocol          = "tcp"
-  from_port         = "1521"
-  to_port           = "1521"
-  self              = true
-  description       = "TF - MIS DB out"
+
+## Apply this SG to MIS to enable connection to Delius DB
+resource "aws_security_group" "mis_out_to_delius_db" {
+  name        = "${var.environment_name}-mis-out-to-delius-db"
+  vpc_id      = "${data.terraform_remote_state.vpc.vpc_id}"
+  description = "MIS out to Delius Database"
+  tags        = "${merge(var.tags, map("Name", "${var.environment_name}-mis-out-to-delius-db", "Type", "DB"))}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+output "sg_mis_out_to_delius_db_id" {
+  value = "${aws_security_group.mis_out_to_delius_db.id}"
+}
+
+resource "aws_security_group_rule" "mis_out_to_delius_db" {
+  security_group_id        = "${aws_security_group.mis_out_to_delius_db.id}"
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = "1521"
+  to_port                  = "1521"
+  source_security_group_id = "${aws_security_group.delius_core_db_in_from_mis.id}"
+  description              = "TF - MIS out to Delius Database"
 }
