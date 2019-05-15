23687bf856f8592ad67e055b7489df18ef0a2b26
smjmoj <smjmoj@gmail.com>
2019-05-15 11:15:14 +0100

    Added LDAP Backups S3 bucket

---
+++ s3/ldap-backups/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "s3" {}
+}
+
+provider "aws" {
+  region  = "${var.region}"
+  version = "~> 1.16"
+}
+
+# ### oracledb-backups-s3bucket
+# S3 Bucket name will have
+# "region-environment_name" prepended
+
+locals {
+  bucket_name = "${var.tiny_environment_identifier}-ldap-backups"
+}
+
+
+resource "aws_s3_bucket" "ldap_backups" {
+  bucket = "${local.bucket_name}"
+  acl    = "private"
+
+  versioning {
+    enabled = false
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = "${merge(var.tags, map("Name", "${local.bucket_name}"))}"
+}
