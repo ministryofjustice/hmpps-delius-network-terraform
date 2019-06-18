14f511d81f2ccad87497799f2a745380e1eb8beb
marcus.aspin <marcus.aspin@bconline.co.uk>
2019-06-18 18:15:42 +0100

    Added EIPs for PWM password self-service tool

--- persistent-eip/delius-elastic-ip-outputs.tf
+++ persistent-eip/delius-elastic-ip-outputs.tf
@@ -63,3 +63,24 @@ output "delius_interface_az3_lb_eip" {
     public_ip     = "${aws_eip.delius_interface_az3_lb.public_ip}"
   }
 }
+
+output "delius_pwm_az1_lb_eip" {
+  value = {
+    allocation_id = "${aws_eip.delius_pwm_az1_lb.id}",
+    public_ip     = "${aws_eip.delius_pwm_az1_lb.public_ip}"
+  }
+}
+
+output "delius_pwm_az2_lb_eip" {
+  value = {
+    allocation_id = "${aws_eip.delius_pwm_az2_lb.id}",
+    public_ip     = "${aws_eip.delius_pwm_az2_lb.public_ip}"
+  }
+}
+
+output "delius_pwm_az3_lb_eip" {
+  value = {
+    allocation_id = "${aws_eip.delius_pwm_az3_lb.id}",
+    public_ip     = "${aws_eip.delius_pwm_az3_lb.public_ip}"
+  }
+}
