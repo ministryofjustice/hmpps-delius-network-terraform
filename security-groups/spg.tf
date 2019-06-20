5b19704228fb983074faf5a622e9ada1e6e4b420
Mark Butler <mark.butler@solirius.com>
2019-06-20 15:53:14 +0100

    Modified the Tag name

--- security-groups/spg.tf
+++ security-groups/spg.tf
@@ -85,7 +85,7 @@ resource "aws_security_group" "spg_amazonmq_in" {
   name        = "${var.environment_name}-delius-core-${var.spg_app_name}-amazonmq-in"
   vpc_id      = "${data.terraform_remote_state.vpc.vpc_id}"
   description = "amazonmq incoming"
-  tags        = "${merge(var.tags, map("Name", "${var.environment_name}_${var.spg_app_name}_api_in", "Type", "API"))}"
+  tags        = "${merge(var.tags, map("Name", "${var.environment_name}_${var.spg_app_name}_amazonmq_in", "Type", "API"))}"
 
   lifecycle {
     create_before_destroy = true
