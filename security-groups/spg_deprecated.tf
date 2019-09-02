3dbf4837ba0462406192530ef9289422bcdf1be8
Paul Crimes <paul.crimes@solirius.com>
2019-09-02 11:57:06 +0100

    reverted changes made before marking as deprecated

--- security-groups/spg_deprecated.tf
+++ security-groups/spg_deprecated.tf
@@ -40,7 +40,17 @@ resource "aws_security_group" "spg_nginx_in" {
   }
 }
 
+# RDS
+resource "aws_security_group" "spg_db_in" {
+  name        = "${var.environment_name}-delius-core-${var.spg_app_name}-db-in"
+  vpc_id      = "${data.terraform_remote_state.vpc.vpc_id}"
+  description = "db incoming"
+  tags        = "${merge(var.tags, map("Name", "${var.environment_name}_${var.spg_app_name}_db_in", "Type", "DB"))}"
 
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
 #API
 # Internal
@@ -66,6 +76,28 @@ resource "aws_security_group" "spg_api_in" {
   }
 }
 
+resource "aws_security_group" "ssh_jenkins_in" {
+  name        = "${var.environment_name}-delius-core-${var.spg_app_name}-ssh-jenkins-in"
+  vpc_id      = "${data.terraform_remote_state.vpc.vpc_id}"
+  description = "ssh access from jenkins"
+  tags        = "${merge(var.tags, map("Name", "${var.environment_name}_${var.spg_app_name}_ssh_jenkins_in", "Type", "API"))}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+resource "aws_security_group_rule" "ssh_jenkins_in" {
+  security_group_id = "${aws_security_group.ssh_jenkins_in.id}"
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = "2222"
+  to_port           = "2222"
+  cidr_blocks       = [ "${data.terraform_remote_state.vpc.eng_vpc_cidr}" ]
+  description       = "TF - ssh_jenkins_in"
+}
+
 
 # OUTPUTS
 # define security groups only for spg outputs
@@ -79,6 +111,10 @@ output "sg_spg_nginx_in" {
   value = "${aws_security_group.spg_nginx_in.id}"
 }
 
+# spg_db_in
+output "sg_spg_db_in" {
+  value = "${aws_security_group.spg_db_in.id}"
+}
 
 # spg_internal_lb_in
 output "sg_spg_internal_lb_in" {
