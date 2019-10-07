1f885a0ab4a6369e117d2dd6657275aa789ec28c
smjmoj <smjmoj@gmail.com>
2019-10-07 15:49:41 +0100

    Added ingress and egress rules for OEM connectivity

--- security-groups/delius-core_mis.tf
+++ security-groups/delius-core_mis.tf
@@ -113,3 +113,64 @@ resource "aws_security_group_rule" "eng_rman_catalog_mis_db_in" {
   source_security_group_id = "${data.terraform_remote_state.ora_db_op_security_groups.sg_map_ids.rman_catalog}"
   description              = "RMAN Catalog in"
 }
+
+# Use same SG for the OEM connections (SG name is not correct any more but this is quick)
+resource "aws_security_group_rule" "mis_db_to_eng_oem_out_22" {
+  security_group_id        = "${aws_security_group.mis_db_in_out_rman_cat.id}"
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = "22"
+  to_port                  = "22"
+  source_security_group_id = "${data.terraform_remote_state.ora_db_op_security_groups.sg_map_ids.oem}"
+  description              = "OEM out 22"
+}
+
+resource "aws_security_group_rule" "eng_oem_mis_db_in_22" {
+  security_group_id        = "${aws_security_group.mis_db_in_out_rman_cat.id}"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = "22"
+  to_port                  = "22"
+  source_security_group_id = "${data.terraform_remote_state.ora_db_op_security_groups.sg_map_ids.oem}"
+  description              = "OEM in 22"
+}
+
+resource "aws_security_group_rule" "mis_db_to_eng_oem_out_1521" {
+  security_group_id        = "${aws_security_group.mis_db_in_out_rman_cat.id}"
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = "1521"
+  to_port                  = "1521"
+  source_security_group_id = "${data.terraform_remote_state.ora_db_op_security_groups.sg_map_ids.oem}"
+  description              = "OEM out 1521"
+}
+
+resource "aws_security_group_rule" "eng_oem_mis_db_in_1521" {
+  security_group_id        = "${aws_security_group.mis_db_in_out_rman_cat.id}"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = "1521"
+  to_port                  = "1521"
+  source_security_group_id = "${data.terraform_remote_state.ora_db_op_security_groups.sg_map_ids.oem}"
+  description              = "OEM in 1521"
+}
+
+resource "aws_security_group_rule" "mis_db_to_eng_oem_out_3872" {
+  security_group_id        = "${aws_security_group.mis_db_in_out_rman_cat.id}"
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = "3872"
+  to_port                  = "3872"
+  source_security_group_id = "${data.terraform_remote_state.ora_db_op_security_groups.sg_map_ids.oem}"
+  description              = "OEM out 3872"
+}
+
+resource "aws_security_group_rule" "eng_oem_mis_db_in_3872" {
+  security_group_id        = "${aws_security_group.mis_db_in_out_rman_cat.id}"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = "3872"
+  to_port                  = "3872"
+  source_security_group_id = "${data.terraform_remote_state.ora_db_op_security_groups.sg_map_ids.oem}"
+  description              = "OEM in 3872"
+}
