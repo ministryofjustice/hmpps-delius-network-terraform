d63a4e035b557ed8f58c778818a75b63f714c2bf
eddie nyambudzi <e.nyambudzi@googlemail.com>
2020-02-19 15:01:22 +0000

    created variables for module options

--- lambda-scheduler/ec2.tf
+++ lambda-scheduler/ec2.tf
@@ -4,11 +4,11 @@ module "ec2-stop-pm" {
   source                         = "modules/lambda-scheduler-stop-start/"
   name                           = "${var.environment_name}-stop-ec2"
   cloudwatch_schedule_expression = "${var.stop_cloudwatch_schedule_expression}"
-  schedule_action                = "stop"
+  schedule_action                = "${var.ec2_schedule_stop_action}"
   spot_schedule                  = "false"
-  ec2_schedule                   = "${var.ec2_schedule_stop_start}"
-  rds_schedule                   = "false"
-  autoscaling_schedule           = "false"
+  ec2_schedule                   = "${var.ec2_enable_schedule}"
+  rds_schedule                   = "${var.rds_schedule}"
+  autoscaling_schedule           = "${var.autoscaling_schedule}"
 
   resources_tag = {
     key   = "auto-stop"
@@ -20,11 +20,11 @@ module "ec2-start-am" {
   source                         = "modules/lambda-scheduler-stop-start/"
   name                           = "${var.environment_name}-start-ec2"
   cloudwatch_schedule_expression = "${var.start_cloudwatch_schedule_expression}"
-  schedule_action                = "start"
-  spot_schedule                  = "false"
-  ec2_schedule                   = "${var.ec2_schedule_stop_start}"
-  rds_schedule                   = "false"
-  autoscaling_schedule           = "false"
+  schedule_action                = "${var.ec2_schedule_start_action}"
+  spot_schedule                  = "${var.spot_schedule}"
+  ec2_schedule                   = "${var.ec2_enable_schedule}"
+  rds_schedule                   = "${var.rds_schedule}"
+  autoscaling_schedule           = "${var.autoscaling_schedule}"
 
   resources_tag = {
     key   = "auto-stop"
--- lambda-scheduler/variables.tf
+++ lambda-scheduler/variables.tf
@@ -4,8 +4,38 @@ variable "region" {
   description = "The AWS region."
 }
 
-variable "start_cloudwatch_schedule_expression" {}
+variable "start_cloudwatch_schedule_expression" {
+  description = "The time to start instances"
+}
 
-variable "stop_cloudwatch_schedule_expression" {}
+variable "stop_cloudwatch_schedule_expression" {
+  description = "The time to stop instances"
+}
 
-variable "ec2_schedule_stop_start" {}
+variable "ec2_enable_schedule" {
+  description = "Enable or disable the auto start and power off of instances"
+  default     = "false"
+}
+
+variable "ec2_schedule_start_action" {
+  description = "Define the schedule option - ie start or stop"
+}
+
+variable "ec2_schedule_stop_action" {
+  description = "Define the schedule option - ie start or stop"
+}
+
+variable "spot_schedule" {
+  description = "Enable or disable auto start/stop of spot instances"
+  default     = "false"
+}
+
+variable "rds_schedule" {
+  description = "Enable or disable auto start/stop of RDS Instances"
+  default     = "false"
+}
+
+variable "autoscaling_schedule" {
+  description = "Enable or disable auto start/stop of ASG"
+  default     = "false"
+}
