9b87d200c4c9544bbc94f9b80a64adb3c7135a07
Martin Spasov <martin.spasov@solirius.com>
2020-03-30 14:57:18 +0100

    Add eips for haproxy

--- persistent-eip/spg-elastic-ip-outputs.tf
+++ persistent-eip/spg-elastic-ip-outputs.tf
@@ -20,3 +20,27 @@ output "spg_az3_lb_eip" {
       public_ip     = "${aws_eip.spg_az3_lb.public_ip}"
     }
 }
+
+
+output "spg_dpg_az1_lb_eip" {
+  value = {
+    allocation_id = "${aws_eip.spg_dpg_az1_lb.id}",
+    public_ip     = "${aws_eip.spg_dpg_az1_lb.public_ip}"
+  }
+}
+
+output "spg_dpg_az2_lb_eip" {
+  value = {
+    allocation_id = "${aws_eip.spg_dpg_az2_lb.id}",
+    public_ip     = "${aws_eip.spg_dpg_az2_lb.public_ip}"
+  }
+}
+
+output "spg_dpg_az3_lb_eip" {
+  value = {
+    allocation_id = "${aws_eip.spg_dpg_az3_lb.id}",
+    public_ip     = "${aws_eip.spg_dpg_az3_lb.public_ip}"
+  }
+}
+
+
