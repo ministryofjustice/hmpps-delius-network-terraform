df3db6561150de89d36394a968c9c1b5cdfa74e0
eddie nyambudzi <e.nyambudzi@googlemail.com>
2020-04-02 00:32:16 +0200

    stagger instance start in phases

--- lambda-scheduler/auto_stop_start.tf
+++ lambda-scheduler/auto_stop_start.tf
@@ -3,50 +3,78 @@
 
 ################################################
 #
-#            EC2 Instances
+#            Lambda Modules
 #
 ################################################
-module "ec2-stop-pm" {
-  source                         = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//auto-start//lambda-scheduler-stop-start"
-  name                           = "${var.environment_name}-stop-ec2"
-  cloudwatch_schedule_expression = "${var.stop_cloudwatch_schedule_expression}"
-  schedule_action                = "${var.schedule_stop_action}"
-  spot_schedule                  = "${var.spot_schedule}"
-  ec2_schedule                   = "${var.ec2_schedule}"
-  rds_schedule                   = "${var.rds_schedule}"
-  autoscaling_schedule           = "${var.autoscaling_schedule}"
-  event_rule_enabled             = "${var.auto_stop_rule_enabled}"
 
-  resources_tag = {
-    key   = "autostop-${var.environment_type}"
-    value = "${var.stop_resources_tag}"
-  }
-}
-
-module "ec2-start-am" {
+####Start instances in a staggered order
+module "ec2-start-phase1" {
   source                         = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//auto-start//lambda-scheduler-stop-start"
-  name                           = "${var.environment_name}-start-ec2"
-  cloudwatch_schedule_expression = "${var.start_cloudwatch_schedule_expression}"
+  name                           = "${var.environment_name}-start-ec2-phase1"
   schedule_action                = "${var.schedule_start_action}"
   spot_schedule                  = "${var.spot_schedule}"
   ec2_schedule                   = "${var.ec2_schedule}"
   rds_schedule                   = "${var.rds_schedule}"
   autoscaling_schedule           = "${var.autoscaling_schedule}"
-  event_rule_enabled             = "${var.auto_start_rule_enabled}"
+  environment_name               = "${var.environment_name}"
 
   resources_tag = {
     key   = "autostop-${var.environment_type}"
-    value = "${var.start_resources_tag}"
+    value = "${var.start_resources_tag_phase1}"
   }
 }
 
-module "autostop-notify" {
-  source                         = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//auto-start//auto-stop-notify"
-  name                           = "${var.environment_name}"
-  cloudwatch_schedule_expression = "${var.stop_cloudwatch_notification_schedule_expression}"
-  event_rule_enabled             = "${var.calendar_rule_enabled}"
+module "ec2-start-phase2" {
+  source                         = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//auto-start//lambda-scheduler-stop-start"
+  name                           = "${var.environment_name}-start-ec2-phase2"
+  schedule_action                = "${var.schedule_start_action}"
+  spot_schedule                  = "${var.spot_schedule}"
+  ec2_schedule                   = "${var.ec2_schedule}"
+  rds_schedule                   = "${var.rds_schedule}"
+  autoscaling_schedule           = "${var.autoscaling_schedule}"
+  environment_name               = "${var.environment_name}"
+
+  resources_tag = {
+    key   = "autostop-${var.environment_type}"
+    value = "${var.start_resources_tag_phase2}"
+  }
 }
 
+###Stop instances in a staggered order
+module "ec2-stop-phase1" {
+  source                         = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//auto-start//lambda-scheduler-stop-start"
+  name                           = "${var.environment_name}-stop-ec2-phase1"
+  schedule_action                = "${var.schedule_stop_action}"
+  spot_schedule                  = "${var.spot_schedule}"
+  ec2_schedule                   = "${var.ec2_schedule}"
+  rds_schedule                   = "${var.rds_schedule}"
+  autoscaling_schedule           = "${var.autoscaling_schedule}"
+  environment_name               = "${var.environment_name}"
+
+  resources_tag = {
+    key   = "autostop-${var.environment_type}"
+    value = "${var.stop_resources_tag_phase1}"
+  }
+}
+
+module "ec2-stop-phase2" {
+  source                         = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//auto-start//lambda-scheduler-stop-start"
+  name                           = "${var.environment_name}-stop-ec2-phase2"
+  schedule_action                = "${var.schedule_stop_action}"
+  spot_schedule                  = "${var.spot_schedule}"
+  ec2_schedule                   = "${var.ec2_schedule}"
+  rds_schedule                   = "${var.rds_schedule}"
+  autoscaling_schedule           = "${var.autoscaling_schedule}"
+  environment_name               = "${var.environment_name}"
+
+  resources_tag = {
+    key   = "autostop-${var.environment_type}"
+    value = "${var.stop_resources_tag_phase2}"
+  }
+}
+
+
+###Calendar to schedule instance availability
 module "calendar" {
   source                         = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//auto-start//calendar"
   environment_name               = "${var.environment_name}"
@@ -55,4 +83,13 @@ module "calendar" {
   is_enabled                     = "${var.calendar_rule_enabled}"
   schedule_expression            = "${var.rate_schedule_expression}"
   calender_content_doc           = "${var.calender_content_doc}"
+}
+
+
+###Notify in slack that instance will go down in 60mins
+module "autostop-notify" {
+  source                         = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//auto-start//auto-stop-notify"
+  name                           = "${var.environment_name}"
+  cloudwatch_schedule_expression = "${var.stop_cloudwatch_notification_schedule_expression}"
+  event_rule_enabled             = "${var.calendar_rule_enabled}"
 }
