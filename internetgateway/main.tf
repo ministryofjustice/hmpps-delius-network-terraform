24ebaa2d4754dd7f2e6c482590dbdf8c8dc79201
smjmoj <smjmoj@gmail.com>
2020-06-25 17:33:40 +0100

    Changes made by running "terragrunt 0.12upgrade" for "internetgateway"

--- internetgateway/main.tf
+++ internetgateway/main.tf
@@ -6,16 +6,17 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
 
-  config {
-    bucket = "${var.remote_state_bucket_name}"
+  config = {
+    bucket = var.remote_state_bucket_name
     key    = "vpc/terraform.tfstate"
-    region = "${var.region}"
+    region = var.region
   }
 }
 
 module "env_igw" {
   source       = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git//modules/internetgateway?ref=ALS-884-TF_12Spike_0.1.0_tf0.12"
-  gateway_name = "${var.environment_name}"
-  vpc_id       = "${data.terraform_remote_state.vpc.vpc_id}"
-  tags         = "${var.tags}"
+  gateway_name = var.environment_name
+  vpc_id       = data.terraform_remote_state.vpc.outputs.vpc_id
+  tags         = var.tags
 }
+
