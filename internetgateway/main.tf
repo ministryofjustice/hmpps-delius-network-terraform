1969c76abe4f0f8e2f58da8b79203df5ae21f9ad
smjmoj <smjmoj@gmail.com>
2020-06-25 17:29:42 +0100

    First changes known to enable init with TF0.12 for internetgateway

--- internetgateway/main.tf
+++ internetgateway/main.tf
@@ -1,12 +1,7 @@
-terraform {
-  # The configuration for this backend will be filled in by Terragrunt
-  backend "s3" {}
-}
-
-provider "aws" {
-  region  = "${var.region}"
-  version = "~> 2.65"
-}
+## The configuration for this backend will be filled in by Terragrunt
+## it will create these files from ../terragrunt.hcl
+## provider.tf
+## backend.tf
 
 data "terraform_remote_state" "vpc" {
   backend = "s3"
@@ -19,7 +14,7 @@ data "terraform_remote_state" "vpc" {
 }
 
 module "env_igw" {
-  source       = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//internetgateway"
+  source       = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git//modules/internetgateway?ref=ALS-884-TF_12Spike_0.1.0_tf0.12"
   gateway_name = "${var.environment_name}"
   vpc_id       = "${data.terraform_remote_state.vpc.vpc_id}"
   tags         = "${var.tags}"
