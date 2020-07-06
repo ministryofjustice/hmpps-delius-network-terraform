144d0d9bf498327f7190ee840e22458835eff7bf
Paul Crimes <paul.crimes@solirius.com>
2020-07-06 16:42:24 +0100

    Remove delegated zone

--- vpc-minimal-only/route_53.tf
+++ vpc-minimal-only/route_53.tf
@@ -13,16 +13,16 @@ resource "aws_route53_zone" "strategic_zone" {
   name  = "${local.strategic_public_domain}"
 }
 
-# Delegation record so we can access the strategic route53 zone from prod
-resource "aws_route53_record" "delegation_record" {
-  # TODO once/if public zones are migrated to this strategic zone, prod zone should be managed by TF - this will require an import
-  count = "${var.environment_name != "delius-prod" ? 1 : 0}"
-  # The zone id of the prod R53 zone
-  zone_id = "${var.strategic_parent_zone_id}"
-  name    = "${local.strategic_public_domain}"
-  type    = "NS"
-  ttl     = "300"
-  records = ["${aws_route53_zone.strategic_zone.name_servers}"]
-  # Use alternative provider which assumes cross account role in prod for managing R53 records
-  provider = "aws.delius_prod_acct_r53_delegation"
-}
+//# Delegation record so we can access the strategic route53 zone from prod
+//resource "aws_route53_record" "delegation_record" {
+//  # TODO once/if public zones are migrated to this strategic zone, prod zone should be managed by TF - this will require an import
+//  count = "${var.environment_name != "delius-prod" ? 1 : 0}"
+//  # The zone id of the prod R53 zone
+//  zone_id = "${var.strategic_parent_zone_id}"
+//  name    = "${local.strategic_public_domain}"
+//  type    = "NS"
+//  ttl     = "300"
+//  records = ["${aws_route53_zone.strategic_zone.name_servers}"]
+//  # Use alternative provider which assumes cross account role in prod for managing R53 records
+//  provider = "aws.delius_prod_acct_r53_delegation"
+//}
