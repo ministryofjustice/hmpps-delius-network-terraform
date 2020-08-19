c1a0b79247d349fafef2fd54387769d414a4418e
eddie nyambudzi <e.nyambudzi@googlemail.com>
2020-08-19 10:31:06 +0200

    change bootstrap branch to master

--- smtp-server/user_data/bootstrap.sh
+++ smtp-server/user_data/bootstrap.sh
@@ -54,7 +54,7 @@ cat << EOF > ~/requirements.yml
   src: singleplatform-eng.users
 - name: smtp
   src: https://github.com/ministryofjustice/hmpps-smtp-installer
-  version: ALS_451
+  version: master
 EOF
 
 wget https://raw.githubusercontent.com/ministryofjustice/hmpps-delius-ansible/master/group_vars/${bastion_inventory}.yml -O users.yml
