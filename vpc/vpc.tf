e48e9bf901c60db5386b12c0e8454de3618597bc
Vincent Cheung <vincent.cheung@digital.justice.gov.uk>
2022-05-25 17:06:20 +0100

    Add in gateway endpoint to comm with S3

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,3 +1,16 @@
+locals {
+
+  route_table_for_endpoint_ids = [
+    module.private_subnet_az1.routetableid,
+    module.private_subnet_az2.routetableid,
+    module.private_subnet_az3.routetableid,
+    module.db_subnet_az1.routetableid,
+    module.db_subnet_az2.routetableid,
+    module.db_subnet_az3.routetableid
+  ]
+
+}
+
 module "vpc" {
   source                 = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git//modules/vpc?ref=terraform-0.12"
   vpc_name               = var.environment_name
@@ -7,3 +20,10 @@ module "vpc" {
   tags                   = var.tags
 }
 
+resource "aws_vpc_endpoint" "s3-endpoint" {
+  service_name      = "com.amazonaws.eu-west-2.s3"
+  vpc_id            = module.vpc.vpc_id
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = local.route_table_for_endpoint_ids
+  tags              = var.tags
+}
