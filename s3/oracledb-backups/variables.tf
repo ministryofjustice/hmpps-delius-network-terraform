4ef2f20701560f0cf44d413f68d0ee0c89407d32
ranbeersingh1 <ranbeersinghtradedata@hotmail.com>
2023-08-30 11:39:58 +0100

    Oracle s3 backup bucket access from modernisation platform

--- s3/oracledb-backups/variables.tf
+++ s3/oracledb-backups/variables.tf
@@ -10,3 +10,10 @@ variable "region" {
   description = "The AWS region."
 }
 
+variable "oracle_s3_backup_bucket_access" {
+  type    = map(string)
+  default = {
+    modernisation_platform_role_arn = ""
+  }
+}
+
