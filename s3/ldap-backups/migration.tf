24e525f7280d4c052439e2bedac9aad7d2738609
Prem Basumatary <prem.basumatary@digital.justice.gov.uk>
2024-11-22 10:28:21 +0000

    TM-724 prep for ldap migration dryrun in preprod

--- s3/ldap-backups/migration.tf
+++ s3/ldap-backups/migration.tf
@@ -84,8 +84,12 @@ resource "aws_lambda_function" "data_transfer_lambda" {
   role          = aws_iam_role.lambda_role.arn
   handler       = "${local.lambda_name}.handler"
   runtime       = "python3.8"
-  memory_size   = 5120
-  timeout       = 300
+  memory_size   = 10240
+  timeout       = 900
+  ephemeral_storage {
+    size = 10240
+  }
+
   source_code_hash = data.archive_file.data_transfer_lambda.output_base64sha256
 
   # Environment variables
